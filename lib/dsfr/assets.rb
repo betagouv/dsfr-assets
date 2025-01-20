c73ae3dcb3ace68dfdc12df19f2eee1ecb4de727
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2025-01-20 19:12:24 +0100

    rails: add the Rails engine

--- lib/dsfr/assets.rb
+++ lib/dsfr/assets.rb
@@ -4,7 +4,11 @@ require_relative "assets/version"
 
 module Dsfr
   module Assets
-    class Error < StandardError; end
-    # Your code goes here...
+    class Engine < ::Rails::Railtie
+      initializer "dsfr-assets.paths" do |app|
+        app.config.assets.paths << File.expand_path("../../vendor/assets/stylesheets", __dir__)
+        app.config.assets.paths << File.expand_path("../../vendor/assets/javascripts", __dir__)
+      end
+    end
   end
 end
