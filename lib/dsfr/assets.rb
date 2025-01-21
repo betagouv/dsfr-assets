e8daa25b916f3467205378c07a7aaecb80fc2066
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2025-01-21 11:58:15 +0100

    lib: don't include the engine if Rails isn't present

--- lib/dsfr/assets.rb
+++ lib/dsfr/assets.rb
@@ -2,13 +2,4 @@
 
 require_relative "assets/version"
 
-module Dsfr
-  module Assets
-    class Engine < ::Rails::Railtie
-      initializer "dsfr-assets.paths" do |app|
-        app.config.assets.paths << File.expand_path("../../vendor/assets/stylesheets", __dir__)
-        app.config.assets.paths << File.expand_path("../../vendor/assets/javascripts", __dir__)
-      end
-    end
-  end
-end
+require_relative "assets/railtie" if defined?(Rails::Railtie)
